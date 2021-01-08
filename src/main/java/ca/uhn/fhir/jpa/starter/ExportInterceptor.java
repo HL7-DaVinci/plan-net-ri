575f56072c2761fd535d6e24f940aad21e33052c
Benjamin Langley <blangley@mitre.org>
2021-01-08 09:49:29 -0500

    Fix Content-Location spellingerror

--- src/main/java/ca/uhn/fhir/jpa/starter/ExportInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/ExportInterceptor.java
@@ -18,7 +18,7 @@ public class ExportInterceptor extends InterceptorAdapter {
      String endPoint = theRequest.getRequestURL().substring(theRequest.getRequestURL().lastIndexOf("/")+1);
      if (endPoint.equals("$export") && theRequest.getMethod().equals("GET")) {
          theResponse.setStatus(202);
-         theResponse.setHeader("Conent-Location", "http://davinci-plan-net-ri.logicahealth.org/resources/export.json");
+         theResponse.setHeader("Content-Location", "http://davinci-plan-net-ri.logicahealth.org/resources/export.json");
         return false;
      }
       return true;
